c3aa8eb0df777f19effa248024b253e4e6b0db00
Daniel Waltrip <dwaltrip77@gmail.com>
2025-07-24 12:56:28 -0700

    setup tailwind

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
 export function HomePage() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-3xl font-bold underline text-gray-800">
+        Home
+      </h1>
       <p>Welcome to the home page</p>
     </div>
   );
