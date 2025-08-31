d9cd9c33183fd540c7e8e3b982b01deb98b45799
Daniel Waltrip <dwaltrip77@gmail.com>
2025-08-31 11:23:38 -0700

    Random game config stuff, tweaking to help w/ refactor

--- core/src/default-game-config.ts
+++ core/src/default-game-config.ts
@@ -2,9 +2,12 @@ import type { GameGenerationConfig } from '@core/game-generation-config';
 
 const DEFAULT_GAME_GENERATION_CONFIG: GameGenerationConfig = {
   // mapSize: { width: 15, height: 15 },
-  mapSize: { width: 30, height: 25 },
+  // mapSize: { width: 30, height: 25 },
+  mapSize: { width: 20, height: 20 },
+  // mapSize: { width: 5, height: 5 },
   mountainDensity: 0.1,
-  minGeneralDistance: 10,
+  // minGeneralDistance: 10,
+  minGeneralDistance: 3,
 };
 
 export { DEFAULT_GAME_GENERATION_CONFIG };
