970ea4e805f26b96c27015e510f30d9dfe7bcea6
Daniel Waltrip <dwaltrip77@gmail.com>
2026-02-03 23:39:37 -0800

    Tweak timing for key repeat

--- apps/frontend/src/settings/user-interaction-config.ts
+++ apps/frontend/src/settings/user-interaction-config.ts
@@ -1,5 +1,5 @@
 // TODO: tweaking these values... may need to fix perf issues first
 // export const KEY_REPEAT_RATE_MS = 100;
-export const KEY_REPEAT_RATE_MS = 50;
+export const KEY_REPEAT_RATE_MS = 30;
 // export const KEY_REPEAT_INITIAL_DELAY_MS = 300;
-export const KEY_REPEAT_INITIAL_DELAY_MS = 150;
+export const KEY_REPEAT_INITIAL_DELAY_MS = 100;
