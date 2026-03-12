4b9036386ce272796f277b2c401821c43bbed506
Daniel Waltrip <dwaltrip77@gmail.com>
2026-03-12 09:02:42 -0700

    Tweaking the UI selected tiles, possible moves, etc

--- apps/frontend/src/domains/sandbox/actions/undo-move.ts
+++ apps/frontend/src/domains/sandbox/actions/undo-move.ts
@@ -1,6 +1,8 @@
 import { sandboxWsEffects } from '@/domains/sandbox/ws-effects';
+import { undoLastQueuedMove } from '@/domains/games/board-store';
 
 function undoMove(): void {
+  undoLastQueuedMove();
   sandboxWsEffects.sendUndoMove();
 }
 
