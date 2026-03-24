5a4aa3eaa1fc75e22adb369b227d1d22da59f302
Daniel Waltrip <dwaltrip77@gmail.com>
2026-03-24 09:24:11 -0700

    solver-ui: path aliases

--- apps/solver-ui/vite.config.ts
+++ apps/solver-ui/vite.config.ts
@@ -1,7 +1,18 @@
+import path from 'path';
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@utils': path.resolve(__dirname, '../../packages/utils'),
+      '@core': path.resolve(__dirname, '../../packages/core/src'),
+      '@algos': path.resolve(__dirname, '../../packages/algos/src'),
+    },
+  },
 });
